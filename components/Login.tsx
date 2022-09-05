a9979e462ed9f42bcc2624dafd3d4f8ba896f6cd
Rick Brown <ricky.brown.00@gmail.com>
2022-09-05 23:30:38 +0100

    adds Login component

---
+++ components/Login.tsx
@@ -0,0 +1,41 @@
+import { ChatIcon } from '@chakra-ui/icons'
+import { Box, Button, Center, Stack } from '@chakra-ui/react'
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const Login: NextPage = () => {
+	return (
+		<>
+			<Head>
+				<title>Login</title>
+			</Head>
+
+			<Center h='100vh'>
+				<Stack
+					align='center'
+					bgColor='gray.300'
+					p={16}
+					rounded='3xl'
+					spacing={12}
+					boxShadow='lg'
+				>
+					<Box
+						bgGradient='linear(to-r, #7928CA, #FF0080)'
+						w='fit-content'
+						p={5}
+						rounded='3xl'
+						boxShadow='md'
+					>
+						<ChatIcon w='100px' h='100px' color='white' />
+					</Box>
+
+					<Button bgColor='gray.100' boxShadow='md'>
+						Sign In With Google
+					</Button>
+				</Stack>
+			</Center>
+		</>
+	)
+}
+
+export default Login
